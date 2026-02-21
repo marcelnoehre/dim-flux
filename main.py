1da5063ddab654bd426ca50c9216dbc2d642cee0
marcelnoehre <marcelnoehre@web.de>
2026-02-21 15:53:42 +0100

    main script for drawings

--- main.py
+++ main.py
@@ -1,5 +1,55 @@
+from src.utils.visualize import *
+from src.utils.variables import Variables
+from src.fdp.sup_inf import SupInfGraph
+from src.fdp.init_layout import InitLayout
+from src.fdp.forces import ForceDirectedPlacement
+
 def main():
-    print("fca-bipolar-fdp!")
+    cxt = input('File Number:')
+    vars = Variables(cxt, {
+        'plot_si_graph': True,
+        'si_graph_annotations':  False,
+        'plot_initial_layout':  True,
+        'initial_layout_annotations':  False,
+        'plot_optimized_layout':  True,
+        'optimized_layout_annotations':  False,
+        'plot_individual_forces':  True,
+        'plot_combined_forces':  False,
+        'plot_gradients':  False,
+        'plot_origin':  False
+    })
+
+    # Sup-Inf Graph
+    sup_inf_graph = SupInfGraph(vars)
+    vars.scalars = sup_inf_graph.scalars
+    vars.order = sup_inf_graph.order
+    vars.d_si_points = sup_inf_graph.d_si_points
+    vars.n_1 = sup_inf_graph.n_1
+    vars.n_2 = sup_inf_graph.n_2
+
+    if vars.args.plot_si_graph:
+        plot_si_graph(vars)
+
+    # Initial Layout
+    initial_layout = InitLayout(vars)
+    vars.base_vectors = initial_layout.base_vectors
+    vars.coordinates = initial_layout.coordinates
+
+    if vars.args.plot_initial_layout:
+        plot_lattice(vars, 'Initial layout', vars.args.initial_layout_annotations, False)
+
+    # Optimize Layout
+    forces = ForceDirectedPlacement(vars)
+    vars.coordinates = forces.coordinates
+    vars.final_forces = forces.final_forces
+
+    if vars.args.plot_optimized_layout:
+        plot_lattice(
+            vars, 'Optimized layout', vars.args.optimized_layout_annotations,
+            vars.args.plot_individual_forces or vars.args.plot_combined_forces or vars.args.plot_gradients
+        )
+
+    pdf_export(vars, 'm4')
 
 
 if __name__ == "__main__":
